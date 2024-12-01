4e7fe2598c9d141325fa86c1551a182deb161f5b
Sobhan <46265207+pyrovetis@users.noreply.github.com>
2024-12-01 21:00:06 +0330

    Create 561.py

---
+++ 561.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        result = 0
+
+        for i in range(0, len(nums), 2):
+            result += nums[i]
+
+        return result
